462267e67a3427b435015a30530d1780b70b4f9a
Anna Nedelkina <annanedelkina@MacBook-Pro.local>
2022-04-05 22:48:47 +0300

    null check

--- Task2.1/MatrixHelper.cs
+++ Task2.1/MatrixHelper.cs
@@ -4,10 +4,13 @@ public static class MatrixHelper
 {
     public static int[] Sum(this Matrix m1, Matrix m2)
     {
+        m1 ??= new Matrix(0);
+        m2 ??= new Matrix(0);
+
         var maxSize = Math.Max(m1.Size, m2.Size);
         var newArray = new int[maxSize];
 
-        for(int i=0;i<maxSize;i++)
+        for (int i = 0; i < maxSize; i++)
         {
             newArray[i] = m1[i, i] + m2[i, i];
         }
--- Task2.2/Program.cs
+++ Task2.2/Program.cs
@@ -3,7 +3,6 @@
 var l1 = new Lection(topic:"topic 1");
 var pr1 = new PracticalLesson("description 1", "Condition 1", "Solution 1");
 var pr2 = new PracticalLesson();
-
 var tr = new Training(pr2, pr1);
 
 Console.WriteLine(tr.IsPractical());
@@ -11,5 +10,3 @@ Console.WriteLine(tr.IsPractical());
 tr.Add(l1);
 
 Console.WriteLine(tr.IsPractical());
-
-var k = tr.Clone();
